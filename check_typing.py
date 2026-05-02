85ad1f28d9dbf29dc8fe63c9cd94a940313d87b2
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2026-05-02 03:17:13 +0200

    Add type hinting (#216)
    * Convert to directory layout
    
    * Add .pyi file
    
    * Add workflow to check type stubs with stubtest
    
    * Add mypy type checking to CI workflow
    
    * Add more typing info, add a test file for type checking
    
    * Make annotations compatible with Python 3.10
    
    * Fix errors in type-stubs job
    
    * Fix Module "typing" has no attribute "assert_type"
    
    * Add typing-extensions to test requirements
    
    * pytest: ignore test_typing.py
    
    * Rename and move test_typing so that it is ignored by pytest under all circumstances.
    
    * Update `format: str` in `Parser.__init__`
    
    Co-authored-by: Wim Jeantine-Glenn <wim.glenn@gmail.com>
    
    * Add `compile`
    
    ---------
    
    Co-authored-by: Wim Jeantine-Glenn <wim.glenn@gmail.com>

---
+++ check_typing.py
@@ -0,0 +1,79 @@
+# This file contains tests that check the type annotations in parse/__init__.pyi.
+# It is not to be run with pytest, but is instead meant to be checked with mypy.
+
+# Use typing_extensions because we want to be compatible with Python 3.10
+from typing_extensions import assert_type
+
+
+from parse import Parser, Result, Match, parse, search, findall, ResultIterator
+
+# NB: When checking for ResultIterator[Result], we need to use a string literal,
+# because the "real" ResultIterator is not a generic type at runtime.
+
+
+def test_parse() -> None:
+    result = parse("{:d} {:d}", "123 456")
+    assert_type(result, Result | None)
+
+    result = parse("{:d} {:d}", "123 456", evaluate_result=True)
+    assert_type(result, Result | None)
+
+    match = parse("{:d} {:d}", "123 456", evaluate_result=False)
+    assert_type(match, Match | None)
+
+
+def test_search() -> None:
+    result = search("{:d} {:d}", "123 456")
+    assert_type(result, Result | None)
+
+    result = search("{:d} {:d}", "123 456", evaluate_result=True)
+    assert_type(result, Result | None)
+
+    match = search("{:d} {:d}", "123 456", evaluate_result=False)
+    assert_type(match, Match | None)
+
+
+def test_findall() -> None:
+    result_iterator = findall("{:d} {:d}", "123 456 789 012")
+    assert_type(result_iterator, "ResultIterator[Result]")
+
+    result_iterator = findall("{:d} {:d}", "123 456 789 012", evaluate_result=True)
+    assert_type(result_iterator, "ResultIterator[Result]")
+
+    match_iterator = findall("{:d} {:d}", "123 456 789 012", evaluate_result=False)
+    assert_type(match_iterator, "ResultIterator[Match]")
+
+
+def test_parser() -> None:
+    parser = Parser("{a:d} {:d}")
+
+    result = parser.parse("123 456")
+    assert_type(result, Result | None)
+
+    result = parser.parse("123 456", evaluate_result=True)
+    assert_type(result, Result | None)
+
+    match = parser.parse("123 456", evaluate_result=False)
+    assert_type(match, Match | None)
+
+    result = parser.search("123 456")
+    assert_type(result, Result | None)
+
+    result = parser.search("123 456", evaluate_result=True)
+    assert_type(result, Result | None)
+
+    match = parser.search("123 456", evaluate_result=False)
+    assert_type(match, Match | None)
+
+    result_iterator = parser.findall("123 456 789 012")
+    assert_type(result_iterator, "ResultIterator[Result]")
+
+    result_iterator = parser.findall("123 456 789 012", evaluate_result=True)
+    assert_type(result_iterator, "ResultIterator[Result]")
+
+    match_iterator = parser.findall("123 456 789 012", evaluate_result=False)
+    assert_type(match_iterator, "ResultIterator[Match]")
+
+    assert_type(parser.format, str)
+    assert_type(parser.fixed_fields, list[int])
+    assert_type(parser.named_fields, list[str])
